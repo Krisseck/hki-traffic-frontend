9b763f7dcb785fe83877c57d0c46cbe894cb62eb
Kristian Polso <kristian.polso@gmail.com>
2018-07-23 15:42:00 +0300

    Added Bulma

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
     <div id="nav-toggle"><img src="data:image/svg+xml;utf8;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iaXNvLTg4NTktMSI/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHZlcnNpb249IjEuMSIgdmlld0JveD0iMCAwIDI0IDI0IiBlbmFibGUtYmFja2dyb3VuZD0ibmV3IDAgMCAyNCAyNCIgd2lkdGg9IjUxMnB4IiBoZWlnaHQ9IjUxMnB4Ij4KICA8Zz4KICAgIDxwYXRoIGQ9Ik0yNCwzYzAtMC42LTAuNC0xLTEtMUgxQzAuNCwyLDAsMi40LDAsM3YyYzAsMC42LDAuNCwxLDEsMWgyMmMwLjYsMCwxLTAuNCwxLTFWM3oiIGZpbGw9IiMwMDAwMDAiLz4KICAgIDxwYXRoIGQ9Ik0yNCwxMWMwLTAuNi0wLjQtMS0xLTFIMWMtMC42LDAtMSwwLjQtMSwxdjJjMCwwLjYsMC40LDEsMSwxaDIyYzAuNiwwLDEtMC40LDEtMVYxMXoiIGZpbGw9IiMwMDAwMDAiLz4KICAgIDxwYXRoIGQ9Ik0yNCwxOWMwLTAuNi0wLjQtMS0xLTFIMWMtMC42LDAtMSwwLjQtMSwxdjJjMCwwLjYsMC40LDEsMSwxaDIyYzAuNiwwLDEtMC40LDEtMVYxOXoiIGZpbGw9IiMwMDAwMDAiLz4KICA8L2c+Cjwvc3ZnPgo=" v-on:click="menuOpened = !menuOpened" /></div>
-    <div id="nav" v-bind:class="{'opened': menuOpened}">
-      <router-link to="/" v-on:click.native="menuOpened = false" class="router-link">Home</router-link>
-      <router-link to="/about" v-on:click.native="menuOpened = false" class="router-link">About</router-link>
-    </div>
+    <aside class="menu" id="nav" v-bind:class="{'opened': menuOpened}">
+      <ul class="menu-list">
+        <li><router-link to="/" v-on:click.native="menuOpened = false" class="router-link">Home</router-link></li>
+        <li><router-link to="/about" v-on:click.native="menuOpened = false" class="router-link">About</router-link></li>
+      </ul>
+    </aside>
     <router-view/>
   </div>
 </template>
@@ -23,7 +25,9 @@ export default {
 
 <style lang="scss">
 
-@import './assets/reset.scss';
+@import '../node_modules/bulma/bulma.sass';
+@import '../node_modules/bulmaswatch/lux/_variables.scss';
+@import '../node_modules/bulmaswatch/lux/_overrides.scss';
 
 strong, b {
   font-weight: bold;
@@ -50,21 +54,10 @@ strong, b {
   right: -20rem;
   width: 20rem;
   height: 100vh;
-  background: white;
-  z-index: 1001;
-  box-shadow: 0px 0px 1rem rgba(0, 0, 0, 0.5);
   &.opened {
     right: 0;
   }
   .router-link {
-    display: block;
-    border-bottom: 1px solid #999;
-    padding: 1rem 0.5rem;
-    text-decoration: none;
-    color: black;
-    &:hover {
-      background: #eee;
-    }
     &.router-link-exact-active {
       color: red;
     }
