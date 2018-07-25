bc75012c66ba00da5bba56caf23a56d3085cebc2
Kristian Polso <kristian.polso@gmail.com>
2018-07-25 12:24:21 +0300

    Add Analytics

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
 
 Vue.filter('formatTime', function(value) {
   return (value < 1000 ? "0" + value/100 + ":00" : value/100 + ":00");
+});
+
+Vue.use(VueAnalytics, {
+  id: 'UA-122843777-1',
+  router
 });
 
 new Vue({
